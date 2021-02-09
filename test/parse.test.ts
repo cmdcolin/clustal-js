46d321f89c7b366f9773a0cce4f878e18ec9f6c3
Colin <colin.diesh@gmail.com>
2021-02-08 18:49:10 -0700

    Minor fix

--- test/parse.test.ts
+++ test/parse.test.ts
@@ -16,6 +16,6 @@ test("a smaller clustal", async () => {
 test("a larger clustal", async () => {
   const ret = fs.readFileSync(require.resolve("./data/out.aln"), "utf8");
   const res = await parse(ret);
-  fs.writeFileSync("out.txt", JSON.stringify(res.alns))
+  fs.writeFileSync("out.txt", JSON.stringify(res.alns));
   expect(res).toMatchSnapshot();
 });
